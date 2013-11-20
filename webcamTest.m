1470a15b6a3ec9e46611fc88696da3d5205715a2
corypisano <cory.pisano@gmail.com>
2013-11-20 16:42:24 -0500

    added smiley alignment

--- webcamTest.m
+++ webcamTest.m
@@ -12,7 +12,8 @@ clear all; close all; clc
 OS = computer();
 switch OS
     case 'PCWIN64' % windows
-        vid = videoinput('winvideo', 1, 'YUY2_320x240');
+        %vid = videoinput('winvideo', 1, 'YUY2_320x240');
+        vid = videoinput('winvideo', 1);
     case 'MACI64'  % mac    
         vid = videoinput('macvideo',1); % add resolution! (need to call "imaqtool" on mac)
     otherwise      % sorry linux   
@@ -23,7 +24,26 @@ end
 vid.FramesPerTrigger = 1; 
 vid.ReturnedColorspace = 'rgb'; % return type. maybe we only need grayscale?
 
-preview(vid); % show real time webcam data
+% get webcam parameters
+xRes = vid.VideoResolution
+yRes = xRes(2);
+xRes = xRes(1);
+xCenter = xRes/2;
+yCenter = yRes/2;
+
+% display webcam with smiley overlay for alignment
+hImage = imshow(zeros(yRes,xRes)); % create figure handle
+preview(vid, hImage);              % show real time webcam data
+hold on;                           % plot face on top the preview
+
+% just a placeholder face for now...
+smiley.eyes = [xCenter-0.07*xRes yCenter-0.1*yRes; ...
+               xCenter+0.07*xRes yCenter-0.1*yRes];  
+smiley.mouth_x = (xCenter-0.1*xRes):(xCenter+0.1*xRes);
+smiley.mouth_y = (yCenter+0.1*yRes)*ones(1,length(smiley.mouth_x));
+plot(smiley.eyes(:,1),smiley.eyes(:,2),'ro','MarkerSize',7);
+plot(smiley.mouth_x,smiley.mouth_y,'r');
+title('Please align your face with the smiley :|');
 
 % wait for input, should be changed to a button press in GUI
 input('Any input to take snapshot:  ','s');
